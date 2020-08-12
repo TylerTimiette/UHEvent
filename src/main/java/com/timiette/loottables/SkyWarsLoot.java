21d1586d9b8cf3aa66b98a0fc42d1198d84923f8
TylerTimiette <69523946+TylerTimiette@users.noreply.github.com>
2020-08-11 21:16:36 -0400

    hsdajd

--- src/main/java/com/timiette/loottables/SkyWarsLoot.java
+++ src/main/java/com/timiette/loottables/SkyWarsLoot.java
@@ -5,11 +5,14 @@ import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.graalvm.compiler.graph.Graph;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SkyWarsLoot extends SeedableGame {
     public HashMap<ItemStack, Double> chestInventory = new HashMap<>();
@@ -49,7 +52,10 @@ public class SkyWarsLoot extends SeedableGame {
         Random random = new Random(getSeed());
         int maxItems = random.nextInt(max - min + 1) + min;
         List<ItemStack> lootList = new ArrayList<ItemStack>(chestInventory.keySet());
-
+        //Thanks Lily!
+        List<Integer> list = IntStream.range(0, 26)
+                .boxed()
+                .collect(Collectors.toList());
         //Picking an item and placing it in a random place in the chest. If a spot is taken, we move it to the next spot.
         for(int i = 0; i <= maxItems; i++) {
             int timesRolled = 0;
@@ -60,19 +66,18 @@ public class SkyWarsLoot extends SeedableGame {
 
             //If we rolled in the right range for the item, we place it in the chest. We have a cap on how many times this is allowed to fail before we override.
             boolean success = RNG >= chestInventory.get(randomItem);
-            if(timesRolled >=5) {
+            if(timesRolled >= 5) {
                 //Manual override
                 success = true;
             }
 
             if(success = true) {
                 //Single chests have only 27 slots.
-                int placement = new Random().nextInt(26);
+
+                int placement = list.get(i);
                 //catch clauses
                 if(inv.getItem(i) != null)
                     placement++;
-                if(placement == 27)
-                    placement = 0;
 
                 inv.setItem(placement, lootList.get(i));
 
